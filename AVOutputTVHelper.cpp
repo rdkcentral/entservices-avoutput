6c9449320d77972e8cf844c2804c655329e3d0b7
aktamilbe <59253707+aktamilbe@users.noreply.github.com>
2026-01-27 16:45:57 +0000

    RDKEMW-12892 :Input Validation for HDRMode

--- AVOutputTVHelper.cpp
+++ AVOutputTVHelper.cpp
@@ -751,8 +751,8 @@ namespace Plugin {
 
         if ( (param == "ColorTemperature") ||
              (param == "DimmingMode") || (param == "AutoBacklightMode") ||
-             (param == "DolbyVisionMode") || (param == "HDR10Mode") ||
-            (param == "HLGMode") || (param == "AspectRatio") || (param == "PictureMode")  ) {
+             (param == "DolbyVisionMode") ||
+            (param == "HDRMode") || (param == "AspectRatio") || (param == "PictureMode")  ) {
             auto iter = find(info.rangeVector.begin(), info.rangeVector.end(), inputValue);
 
             if (iter == info.rangeVector.end()) {
@@ -1905,6 +1905,9 @@ namespace Plugin {
             } else {
                 generateStorageIdentifierWBV2(key, forParam, indexInfo);
             }
+        }
+        else if (m_pictureModeStatus == tvERROR_OPERATION_NOT_SUPPORTED) {
+            generateStorageIdentifier(key, forParam, indexInfo);
         }
         else {
             generateStorageIdentifierV2(key, forParam, indexInfo);
