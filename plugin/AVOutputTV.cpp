9f117f5e9b31ed8e7b74e3f6b1ca20e5eb225894
Anbarasan <anbarasan2710@gmail.com>
2026-08-26 16:25:59 +0530

    Update AVOutputTV.cpp

--- plugin/AVOutputTV.cpp
+++ plugin/AVOutputTV.cpp
@@ -444,7 +444,7 @@ namespace Plugin {
     void AVOutputTV::Initialize()
     {
         LOGINFO("Entry\n");
-#if 0       
+      
         tvError_t ret = tvERROR_NONE;
 
         TR181_ParamData_t param;
@@ -471,7 +471,7 @@ namespace Plugin {
 	else {
             LOGINFO("Platform Init successful...\n");
         }
-
+#if 0
         tvVideoFormatCallbackData callbackData = {this,tvVideoFormatChangeHandler};
         ret = RegisterVideoFormatChangeCB(&callbackData);
         if(ret != tvERROR_NONE) {
@@ -519,7 +519,7 @@ namespace Plugin {
     void AVOutputTV::Deinitialize()
     {
        LOGINFO("Entry\n");
-#if 0
+#if 1
        tvError_t ret = tvERROR_NONE;
        ret = TvTerm();
 
