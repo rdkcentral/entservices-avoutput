1446815373ff2ed68792a4f9be4a4bb06fe5fe8c
aktamilbe <59253707+aktamilbe@users.noreply.github.com>
2026-09-21 16:10:31 +0000

    RDKEVD-1128 : move avop version to 2.0.0

--- plugin/AVOutput.cpp
+++ plugin/AVOutput.cpp
@@ -22,8 +22,8 @@
 #include "UtilsIarm.h"
 #include "UtilsSearchRDKProfile.h"
 
-#define API_VERSION_NUMBER_MAJOR 1
-#define API_VERSION_NUMBER_MINOR 2
+#define API_VERSION_NUMBER_MAJOR 2
+#define API_VERSION_NUMBER_MINOR 0
 #define API_VERSION_NUMBER_PATCH 0
 
 namespace WPEFramework {
